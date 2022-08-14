3b25fac1dbf1efa36bc28c1e52a004a57991e76d
Aditya Shidhaye <ashidhaye2@gmail.com>
2022-08-14 19:08:30 +0530

    HW 3 | Finished HW 3

---
+++ Course/HW3.hs
@@ -0,0 +1,46 @@
+module Golf where
+import Data.List
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+---Exercise 1
+
+skips :: [a] -> [[a]]
+skips lis = go lis 1 (length lis) (zip [1..(length lis)] lis)
+  where 
+    go lis idx len pairList
+      | idx > length lis = []
+      | otherwise = map snd (filter (\(a, b) -> mod a idx == 0) pairList) : go lis (1 + idx) len pairList
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+---Exercise 2
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:zs) =  [y | x < y && y > z] ++ localMaxima (y:z:zs)
+localMaxima _ = []
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+---Exercise 3
+
+constStr :: String
+constStr = "\n==========\n0123456789\n"
+
+mapToCounts :: [Int] -> [Int]
+mapToCounts lis = map (\x -> length $ filter (== x) lis) [0..9]
+
+getEveryLevel :: Int -> Int -> [Int] -> [String]
+getEveryLevel idx maxCnt cnts 
+  | idx > maxCnt = []
+  | otherwise = map (\x -> if idx <= x then '*' else ' ') cnts : getEveryLevel (idx + 1) maxCnt cnts
+
+histogram :: [Int] -> String
+histogram lis = intercalate "\n" (reverse finLis) ++ constStr
+  where 
+    finLis = getEveryLevel 1 (maximum ctrList) ctrList 
+      where 
+        ctrList = mapToCounts lis
