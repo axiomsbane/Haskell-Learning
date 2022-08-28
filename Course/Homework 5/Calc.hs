16fef0d38a6425420c39f1c216909baaf61b7e7f
Aditya Shidhaye <ashidhaye2@gmail.com>
2022-08-28 12:24:52 +0530

    HW 5 | Did till Exercise 4

--- Course/Homework 5/Calc.hs
+++ Course/Homework 5/Calc.hs
@@ -2,6 +2,9 @@ module Calc where
 import ExprT
 import Parser (parseExp)
 
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
 -- data ExprT = Lit Integer
 --            | Add ExprT ExprT
 --            | Mul ExprT ExprT
@@ -19,8 +22,45 @@ go Nothing = Nothing
 evalStr :: String -> Maybe Integer
 evalStr = go . parseExp Lit Add Mul
   
+class Expr a where
+  mul :: a -> a -> a
+  add :: a -> a -> a
+  lit :: Integer -> a
+
+instance Expr ExprT where
+  mul = Mul
+  add = Add
+  lit = Lit
+
+reify :: ExprT -> ExprT
+reify = id
+
+instance Expr Integer where
+  mul x y = x * y
+  add x y = x + y
+  lit = id
+
+instance Expr Bool where
+  mul x y = x && y
+  add x y = x || y
+  lit x 
+    | x <= 0 = False
+    | otherwise = True
+
+instance Expr MinMax where
+  lit x = MinMax x
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+
+instance Expr Mod7 where
+  lit x = Mod7 $ mod x 7
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
 
 
-
-
-
+-- testExp :: Expr a => Maybe a
+-- testExp = parseExp lit add mul "(3 * -4) + (-6)"
+-- testInteger = testExp :: Maybe Integer
+-- testBool = testExp :: Maybe Bool
+-- testMM = testExp :: Maybe MinMax
+-- testSat = testExp :: Maybe Mod7
